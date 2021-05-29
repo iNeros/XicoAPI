bd2ff351a67522fcbc83e306a721f8beee5121d3
gibran <gib.aran5@gmail.com>
2021-05-29 16:16:27 -0500

    update

--- Session.php
+++ Session.php
@@ -1,5 +1,4 @@
 <?php
-    echo "prueba";
     include('db/conopen2.php'); 
     $sql = "SELECT * FROM `alumno` WHERE usuario = '".$_POST['usuario']."' and contraseña = '".$_POST['contraseña']."';";
     $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos");
@@ -15,8 +14,8 @@
            </script>";
     }else{
       session_start();
-      $_SESSION['nombre']=$consulta['nombre'];
+      echo json_encode(session_start();$_SESSION['nombre']=$consulta['nombre'];
       $_SESSION['id_grado']=$consulta['id_grado'];
-      $_SESSION['id_documentos']=$consulta['id_documentos'];
+      $_SESSION['id_documentos']=$consulta['id_documentos'];);
     }	
 ?>
