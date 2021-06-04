a67c58db7d103feebdec759bf6f9d3bfa9bd5cb4
gibran <gib.aran5@gmail.com>
2021-06-03 21:27:30 -0500

    Corrección

--- Alumno.php
+++ Alumno.php
@@ -37,7 +37,7 @@ function permisos() {
         echo json_encode($datos);
         exit();
         }else{//consultar datos de todos los alumnos
-        $sql = "SELECT * FROM alumnos";
+        $sql = "SELECT * FROM alumno";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
@@ -45,7 +45,7 @@ function permisos() {
         }
 }       //inserta datos de alumno en la tabla alumno mediante post
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $sql = "INSERT INTO alumnos VALUES ('".$_POST[id_alumno]."', '".$_POST[nombre]."', '".$_POST[appPat]."', '".$_POST[appMat]."', '".$_POST[usuario]."', '".$_POST[contraseña]."', '".$_POST[id_grado]."', '".$_POST[id_tutor]."', '".$_POST[id_documentos]."')";		  
+        $sql = "INSERT INTO alumno VALUES ('".$_POST[id_alumno]."', '".$_POST[nombre]."', '".$_POST[appPat]."', '".$_POST[appMat]."', '".$_POST[usuario]."', '".$_POST[contraseña]."', '".$_POST[id_grado]."', '".$_POST[id_tutor]."', '".$_POST[id_documentos]."')";		  
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos en post");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
@@ -53,14 +53,14 @@ function permisos() {
         exit();
   }     //update a tabla alumno usando el id_alumno
         if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
-        $sql = "UPDATE alumnos SET `nombre`='".$_GET[nombre]."',`appPat`='".$_GET[appPat]."', `appMat`='".$_GET[appMat]."', `usuario`='".$_GET[usuario]."', `contraseña`='".$_GET[contraseña]."', `id_grado`='".$_GET[id_grado]."', `id_tutor`='".$_GET[id_tutor]."', `id_documentos`='".$_GET[id_documentos]."' WHERE  `id_alumno`='".$_GET[id_alumno]."';";
+        $sql = "UPDATE alumno SET `nombre`='".$_GET[nombre]."',`appPat`='".$_GET[appPat]."', `appMat`='".$_GET[appMat]."', `usuario`='".$_GET[usuario]."', `contraseña`='".$_GET[contraseña]."', `id_grado`='".$_GET[id_grado]."', `id_tutor`='".$_GET[id_tutor]."', `id_documentos`='".$_GET[id_documentos]."' WHERE  `id_alumno`='".$_GET[id_alumno]."';";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos put");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
         exit();
 }       //delete alumno usando el id_alumno
         if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
-        $sql = "DELETE FROM alumnos WHERE  `id_alumno`='".$_GET[id_alumno]."';";
+        $sql = "DELETE FROM alumno WHERE  `id_alumno`='".$_GET[id_alumno]."';";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos delete");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
