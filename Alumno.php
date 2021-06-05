1ed178fc008d19baf060ccaa2850d65a125591d0
gibran <gib.aran5@gmail.com>
2021-06-05 16:53:48 -0500

    Una puta comilla ctm

--- Alumno.php
+++ Alumno.php
@@ -23,7 +23,7 @@ function permisos() {
         //consultar datos del alumno por id_alumno
         if ($_SERVER['REQUEST_METHOD'] == 'GET'){
         if (isset($_GET['User'])) { 
-        $sql = "SELECT * FROM alumno WHERE usuario = '".$_GET['User']."' AND contraseña = '".$_GET['Pass'].'";
+        $sql = "SELECT * FROM alumno WHERE usuario = '".$_GET['User']."' AND contraseña = '".$_GET['Pass']."'";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
