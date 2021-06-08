819be0ad86a7e03dd30655fc650763cd2c3dafa0
iNeros <38114920+iNeros@users.noreply.github.com>
2021-06-08 15:45:16 -0500

    Update DB Conexion
    Los Nuevos Datos De La BD

--- db/conopen2.php
+++ db/conopen2.php
@@ -1,8 +1,8 @@
 <?php
-	$usuario = "xico@xicoclass";
-    $password = "1Proyecto";
-    $servidor = "xicoclass.mysql.database.azure.com";
-    $basededatos = "mydb";
+	$usuario = "u377615737_xico_admin";
+    $password = "1ProyectoXico";
+    $servidor = "151.106.96.1";
+    $basededatos = "u377615737_xico_class";
     $conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
     $db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" ); 
 ?>
