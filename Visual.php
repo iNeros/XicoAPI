465027ee3941be0734af9c3595b680a84f8c874b
michelchable <michel_yafte@hotmail.com>
2021-06-08 20:02:49 -0500

    Update Visual.php

--- Visual.php
+++ Visual.php
@@ -22,8 +22,8 @@ function permisos() {
         
         //consultar datos de visual por id_visual
         if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-        if (isset($_GET['id_grupo'])) { 
-        $sql = "SELECT * FROM visual WHERE  id_grupo = '".$_GET['id_grupo']."'";
+        if (isset($_GET['id_visual'])) { 
+        $sql = "SELECT * FROM visual WHERE  id_visual = '".$_GET['id_visual']."'";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
