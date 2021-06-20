116550b74abfff170ef7c4ae82135e7d684750c0
gibran <gib.aran5@gmail.com>
2021-06-20 00:19:16 -0500

    Update Archivos.php

--- Archivos.php
+++ Archivos.php
@@ -39,7 +39,7 @@ function permisos() {
         }
 }       //inserta archivos en la tabla archivos mediante post
         if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $token = $_POST[ruta]+'&token='+$_POST[token];
+        $token = $_POST[ruta]."&token=".$_POST[token];
         $sql = "INSERT INTO archivos_docentes VALUES (NULL, '".$_POST[nombre]."', '".$token."', '".$_POST[tipo]."', '".$_POST[id_actividades]."')";		  
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos en post 1");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
