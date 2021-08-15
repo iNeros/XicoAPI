0cbb821b41f357973389287620f41b50f62637f3
gibran <gib.aran5@gmail.com>
2021-08-14 22:53:39 -0500

    Update ArchivosAlumnos.php

--- ArchivosAlumnos.php
+++ ArchivosAlumnos.php
@@ -53,7 +53,7 @@ function permisos() {
         exit();
   }     //update a tabla archivos_alumnos usando el id_archivos_alumnos
         if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
-        $sql = "UPDATE archivos_alumnos SET `nombre`='".$_GET[nombre]."',`ruta`='".$_GET[ruta]."',`calificacion`='".$_GET[calificacion]."' WHERE  `id_archivos_alumnos`='".$_GET[id_archivos_alumnos]."';";
+        $sql = "UPDATE archivos_alumnos SET `calificacion`='".$_GET[calificacion]."',`comentario`='".$_GET[comentario]."' WHERE `id_alumno`='".$_GET[id_alumno]."' and `id_actividad`='".$_GET[id_actividad]."';";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos put");
         $datos = mysqli_fetch_all($resultado,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
