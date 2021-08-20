4b4cd46cfefe9cb2f5c87fbdfa5178bb1efac09c
gibran <gib.aran5@gmail.com>
2021-08-20 01:36:53 -0500

    AHORA SI EL FINAL

--- Avisos.php
+++ Avisos.php
@@ -56,6 +56,7 @@ function permisos() {
         while($grupo = mysqli_fetch_array($resultado)){
         $sqls1 = "SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = ".$grupo['id_grupo']." UNION ".$sqls1."";  
         }
+        $sqls1 = "".$sqls1." SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = '0'";
         $resultado1 = mysqli_query($conexion,$sqls1) or die ( "Algo ha ido mal en la consulta a la   base de datos avisos id_docente");
         $datos = mysqli_fetch_all($resultado1,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
