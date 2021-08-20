251344b29b690f89fc1b8a98aff6dbeae69e47f8
gibran <gib.aran5@gmail.com>
2021-08-20 01:12:57 -0500

    Update Mail.php

--- Mail.php
+++ Mail.php
@@ -111,8 +111,8 @@ if(isset($_GET['id_docente'])){
         $sqls1 = "SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = ".$grupo['id_grupo']." UNION ".$sqls1."";  
         }
         echo $sqls1;
-        $sqls2 = "SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 28 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 26 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 25 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 24 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 20 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 2 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 1 UNION";
-        $resultado = mysqli_query($conexion,$sqls2) or die ( "Algo ha ido mal en la consulta a la   base de datos avisos id_docen");
+        $sqls2 = "SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 25 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 24 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 20 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 2 UNION SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = 1 UNION";
+        $resultado = mysqli_query($conexion,$sqls2) or die ( "Algo ha ido mal en la consulta a la   base de datos avisos id_doen");
         echo $resultado;
   
   //echo json_encode($datos);   
