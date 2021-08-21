c6f3385a876e65e408dd2b940f3698bedbe00485
gibran <gib.aran5@gmail.com>
2021-08-21 18:52:41 -0500

    Update Avisos.php

--- Avisos.php
+++ Avisos.php
@@ -54,9 +54,9 @@ function permisos() {
         $sql = "SELECT id_grupo from grupo where id_docente = '".$_GET['id_docente']."'";
         $resultado = mysqli_query($conexion,$sql) or die ( "Algo ha ido mal en la consulta a la   base de datos");
         while($grupo = mysqli_fetch_array($resultado)){
-        $sqls1 = "SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = ".$grupo['id_grupo']." UNION ".$sqls1."";  
+        $sqls1 = "SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo,(SELECT nombre from grupo where id_grupo = ".$grupo['id_grupo'].") AS nombreG from avisos where id_grupo = ".$grupo['id_grupo']." UNION ".$sqls1."";  
         }
-        $sqls1 = "".$sqls1." SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo from avisos where id_grupo = '0'";
+        $sqls1 = "".$sqls1." SELECT avisos.id_avisos,avisos.fecha,avisos.nombre,avisos.id_grupo,(SELECT nombre from grupo where id_grupo = '0') AS nombreG from avisos where id_grupo = '0'";
         $resultado1 = mysqli_query($conexion,$sqls1) or die ( "Algo ha ido mal en la consulta a la   base de datos avisos id_docente");
         $datos = mysqli_fetch_all($resultado1,MYSQLI_ASSOC);
         header("HTTP/1.1 200 OK");
